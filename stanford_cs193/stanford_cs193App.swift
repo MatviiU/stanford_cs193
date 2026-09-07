9cc4c944b49f665e1c2814ba1066294b69da59b6
Matvii <matveyulitskiy@gmail.com>
2026-09-07 18:22:43 +0200

    Initial Commit

---
+++ stanford_cs193/stanford_cs193App.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_cs193App.swift
+//  stanford_cs193
+//
+//  Created by Matvii Ulytskyi on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct stanford_cs193App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
